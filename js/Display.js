dbc0d0cbcdf870f8f2a8eb7dae1e7d6bfa28d1e5
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2020-02-17 15:57:26 -0700

    addDecimal, reset, parse methods

--- js/Display.js
+++ js/Display.js
@@ -19,4 +19,37 @@ export default class Display {
     addValue(input) {
         this.value = this.parse(input);
     }
+
+    addDecimal() {
+        let currentValue = this.value;
+        const DECIMAL = '.'
+        if (!currentValue.includes(DECIMAL)) {
+            this.value = currentValue.concat(DECIMAL);
+        } else {
+            this.value = currentValue;
+        }
+
+    }
+
+    reset() {
+        this.value = 0;
+    }
+
+    parse(input) {
+        let currentValue = this.value;
+
+        // Allow only one zero at the beginning.
+        if (currentValue === '0' && input === '0') {
+            return input;
+        }
+
+        // Allow a decimal number has a zero at the end.
+        if (currentValue.includes('.') && input === '0') {
+            return currentValue.concat(input);
+        }
+
+        // Format value.
+        currentValue = currentValue.split(',').join('');
+        return this.formatOutput(currentValue.concat(input));
+    }
 }
